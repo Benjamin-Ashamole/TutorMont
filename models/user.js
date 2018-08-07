4b77bd16a5697af0811893f63f062e181f0f9c04
obiebryant <41204327+obiebryant@users.noreply.github.com>
2018-08-07 18:34:03 -0400

    Doesn't rehash password anymore

--- models/user.js
+++ models/user.js
@@ -21,6 +21,9 @@ const UserSchema = new Schema ({
 
 UserSchema.pre('save', function(next) {
     let user = this;
+    
+    if (!user.isModified('password')) 
+    return next();
   
     bcrypt.hash(user.password, 10, function (err, hash){
       if (err) return next(err);

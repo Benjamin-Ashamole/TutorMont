786baaac3d94f20a1047f7541fa74eba78489d4a
obiebryant <41204327+obiebryant@users.noreply.github.com>
2018-09-04 19:44:58 -0400

    Slight Update to Css and reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -16,48 +16,15 @@ router.get('/new', auth.requireLogin, (req, res, next) => {
 router.post('/', auth.requireLogin, (req, res, next) => {
     let review = new Review(req.body);
     review.tutorId = req.params.userId
-  
+
     review.save(function(err, review) {
       if(err) { console.error(err) };
-  
+
       return res.redirect( `reviews`);
     });
   });
-<<<<<<< HEAD
 
-  //review show
-  // router.get('/:id', auth.requireLogin, (req, res, next) => {
-  //   Review.findById(req.params.id, function(err, review) {
-  //     if(err) { console.error(err) };
-  
-  //       Post.find({ room: room}).populate('comments').exec(function(err, posts) {
-  //       if(err) { console.error(err) };
-  
-  //        res.render('rooms/show', { room: room, posts: posts });
-  //      });
-  //   });
-  // });
-
-  // router.get('/:id', auth.requireLogin, (req, res, next) => {
-    // User.findById().then((user) => {
-    //   return Review.find({ });
-    // }).then((reviews) => {
-    //   res.render()
-    // }).catch(() => {
-    //   console.log()
-    // })
-
-
-  //   console.log('>>>>> Get reviews for user', req.params.id)
-  //    User.findById(req.params.id, (err, user) => {
-  //      if (err) { console.error(err) };
-  //      console.log('>>>>>> Found a user', user);
-      
-  //   });
-  
-=======
 // review show  
->>>>>>> imgupload
   router.get('/', auth.requireLogin, (req, res, next) => {
     Review.find({ tutorId: req.params.userId },  (err, reviews) => {
     if (err) { console.error(err) };

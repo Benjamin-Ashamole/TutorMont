305e2b6f72348515d554a21c72c6f96cce2f300d
obiebryant <41204327+obiebryant@users.noreply.github.com>
2018-09-04 21:01:25 -0400

    More minor changes

--- models/user.js
+++ models/user.js
@@ -8,7 +8,6 @@ const UserSchema = new Schema ({
      first: { type: String, required: true},
      last: { type: String, required: true},
      email: { type: String, required: true, unique: true },
-    username: { type: String, required: true, unique: true},
     password: { type: String, required: true },
     isVerified: {type: Boolean, default: false},
     passwordResetToken: {type: String},
